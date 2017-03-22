b20c9466a7cd89d0e7ec40e318a15c0889f1c67c
Juan Carlos Ramires <jcramires851@gmail.com>
2017-03-22 19:41:46 -0300

    es6 import export

--- bot.js
+++ bot.js
@@ -1,16 +1,10 @@
-'use strict'
-
-const danbooru = require('danbooru')
 const token = process.env.BOT_TOKEN
 
+import { getRandomInt, checkIfMinutesHavePassed } from 'utils'
+
+const danbooru = require('danbooru')
 const Tgfancy = require('tgfancy')
 
-// if(process.env.NODE_ENV === 'production') {
-//   botInstance = new Bot(token)
-//   console.log('webhook', process.env.WEB_ROOT + botInstance.token)
-//   botInstance.setWebHook(process.env.WEB_ROOT + botInstance.token, './public.pem');
-// }
-// else {
 const botInstance = new Tgfancy(token, {
     tgfancy: {
         emojification: true,
@@ -21,11 +15,7 @@ const botInstance = new Tgfancy(token, {
     }
 })
 
-// }
-
 const db = require('./db')
-
-const utils = require('./utils')
 
 db.connect( err => {
     if (err) {
@@ -33,7 +23,6 @@ db.connect( err => {
         process.exit(1)
     }
 
-    db.get().createCollection('quotes')
     db.get().createCollection('timers')
 
     const timers = db.get().collection('timers')
@@ -49,16 +38,6 @@ db.connect( err => {
 console.log('botInstance server started...')
 
 let timeLastCommandUsed
-
-function checkIfMinutesHavePassed(lastTimeUsed, minutes) {
-    if (lastTimeUsed != undefined) {
-        if (Math.floor((new Date() - lastTimeUsed) / 60000) < minutes) {
-            return false
-        }
-    }
-
-    return true
-}
 
 function checkCommandCooldown(commandCode) {
     if (checkIfMinutesHavePassed(timeLastCommandUsed, 1)) {
@@ -99,39 +78,11 @@ botInstance.onText(/^\/danbooru((\s\w+)+)$/i, (msg, match) => {
     }
 })
 
-// botInstance.onText(/^\/add_quote\s(.*)$/i, (msg, match) => {
-//     let criador
-//     if (msg.from.username) {
-//         criador = msg.from.username
-//     } else {
-//         criador = msg.from.first_name
-//     }
-//
-//     let quote = {
-//         mensagem: match[1],
-//         criador: criador,
-//         criada: Date.now()
-//     }
-//
-//     let collection = db.get().collection('quotes')
-//     collection.insertOne(quote)
-// })
-//
-// botInstance.onText(/^\/quote$/i, (msg, match) => {
-//     let collection = db.get().collection('quotes')
-//     collection.find({}).toArray().then((data) => {
-//         if(data){
-//             let quote = utils.getRandomItemFromList(data)
-//             botInstance.sendMessage(msg.chat.id, quote.mensagem)
-//         }
-//     })
-// })
-
 botInstance.onText(/loli/i, msg => {
     if (checkCommandCooldown('loli')) {
-        utils.getRandomInt(1,2)
+        getRandomInt(1,2)
 
-        switch (utils.getRandomInt(1,2)) {
+        switch (getRandomInt(1,2)) {
             case 1:
                 botInstance.sendSticker(msg.chat.id, './stickers/cocabird.webp', { reply_to_message_id: msg.message_id })
                 break
@@ -173,9 +124,9 @@ botInstance.onText(/tengu/i, msg => {
 botInstance.onText(/korean/i, msg => {
     if (checkCommandCooldown('korean')) {
         if (probability(20)) {
-            utils.getRandomInt(1,2)
+            getRandomInt(1,2)
 
-            switch (utils.getRandomInt(1,2)) {
+            switch (getRandomInt(1,2)) {
                 case 1:
                     botInstance.sendSticker(msg.chat.id, './stickers/anime_noose.webp', { reply_to_message_id: msg.message_id })
                     break
--- utils.js
+++ utils.js
@@ -1,9 +1,13 @@
-'use strict'
-
-exports.getRandomInt = function(min, max) {
+export function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-exports.getRandomItemFromList = function(items) {
-    return items[Math.floor(Math.random() * items.length)]
+export function checkIfMinutesHavePassed(lastTimeUsed, minutes) {
+    if (lastTimeUsed != undefined) {
+        if (Math.floor((new Date() - lastTimeUsed) / 60000) < minutes) {
+            return false
+        }
+    }
+
+    return true
 }
