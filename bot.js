f38bb8061f89b944636ddee88bbd2b3a6cb808d8
Odilon Junior <osousa@stefanini.com>
2017-03-24 11:37:47 -0300

    Crashando ele

--- bot.js
+++ bot.js
@@ -111,7 +111,7 @@ botInstance.onText(/psx/i, msg => {
 
 botInstance.onText(/nojo/i, msg => {
     checkCommandCooldown('nojo', () => {
-        botInstance.sendDocument(msg.chat.id, './imagens/nojo.png', { reply_to_message_id: msg.message_id })
+        botInstance.sendPhoto(msg.chat.id, './imagens/nojo.png', { reply_to_message_id: msg.message_id })
 
 
     })
