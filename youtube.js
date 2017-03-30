b3bfcb378bb1b07e4520bc3e02b72016f47f57eb
Juan Carlos Ramires <jcramires851@gmail.com>
2017-03-30 16:29:10 -0300

    fix mongolices

--- youtube.js
+++ youtube.js
@@ -27,26 +27,6 @@ exports.getLastVideo = (msg, botInstance) => {
     })
 }
 
-const configStatusMongolices = {
-    part: 'status',
-    playlistId: process.env.YOUTUBE_PLAYLIST_ID,
-    key: process.env.YOUTUBE_API_KEY
-}
-function getStatusMongolice() {
-    let response
-    https.get(`https://www.googleapis.com/youtube/v3/playlistItems/?${stringify(configStatusMongolices)}`, result => {
-        let data = ''
-        result.on('error', err => console.log(err))
-        result.on('data', data_ => data += data_)
-        result.on('end', () => {
-            console.log(data)
-            response = JSON.parse(data)
-        })
-    })
-
-    return response
-}
-
 exports.getMongolice = (msg, botInstance) => {
     const configStatusMongolices = {
         part: 'status',
@@ -60,15 +40,18 @@ exports.getMongolice = (msg, botInstance) => {
         result.on('end', () => {
             const status = JSON.parse(data)
 
-            const pages = Math.floor(status.pageInfo.totalResults)
+            const pages = Math.floor(status.pageInfo.totalResults / 50)
             const randomPage = getRandomInt(1, pages)
+
             const config = {
                 part: 'contentDetails',
                 playlistId: process.env.YOUTUBE_PLAYLIST_ID,
-                key: process.env.YOUTUBE_API_KEY
+                key: process.env.YOUTUBE_API_KEY,
+                maxResults: 50
             }
             let pageToken
             for (let i = 0; i < randomPage; i = i + 1) {
+                let messageSent = false
                 config.pageToken = pageToken
                 https.get(`https://www.googleapis.com/youtube/v3/playlistItems/?${stringify(config)}`, response => {
                     let data = ''
@@ -79,11 +62,14 @@ exports.getMongolice = (msg, botInstance) => {
                         if (i == randomPage - 1) {
                             const randomVideoNumber = getRandomInt(1, result.items.length)
                             botInstance.sendMessage(msg.chat.id, `https://www.youtube.com/watch?v=${result.items[randomVideoNumber].contentDetails.videoId}`)
+                            messageSent = true
                         } else {
                             pageToken = result.nextPageToken
                         }
                     })
                 })
+
+                if (messageSent) { break }
             }
         })
     })
