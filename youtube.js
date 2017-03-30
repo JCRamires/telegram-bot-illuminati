1d1dc1c7c32e04bcfb82f4dc868c0db7d36acdd0
Juan Carlos Ramires <jcramires851@gmail.com>
2017-03-30 15:43:32 -0300

    gambi

--- youtube.js
+++ youtube.js
@@ -48,32 +48,43 @@ function getStatusMongolice() {
 }
 
 exports.getMongolice = (msg, botInstance) => {
-    const status = getStatusMongolice()
-    console.log(status)
-    const pages = Math.floor(status.pageInfo.totalResults)
-    const randomPage = getRandomInt(1, pages)
-    const config = {
-        part: 'contentDetails',
+    const configStatusMongolices = {
+        part: 'status',
         playlistId: process.env.YOUTUBE_PLAYLIST_ID,
         key: process.env.YOUTUBE_API_KEY
     }
-    let pageToken
-    for (let i = 0; i < randomPage; i = i + 1) {
-        config.pageToken = pageToken
-        https.get(`https://www.googleapis.com/youtube/v3/playlistItems/?${stringify(config)}`, response => {
-            let data = ''
-            response.on('error', err => console.log(err))
-            response.on('data', data_ => data += data_)
-            response.on('end', () => {
-                const result = JSON.parse(data)
-                if (i == randomPage - 1) {
-                    const randomVideoNumber = getRandomInt(1, result.items.length)
-                    botInstance.sendMessage(msg.chat.id, `https://www.youtube.com/watch?v=${result.items[randomVideoNumber].contentDetails.videoId}`)
-                } else {
-                    pageToken = result.nextPageToken
-                }
-            })
-        })
-    }
+    https.get(`https://www.googleapis.com/youtube/v3/playlistItems/?${stringify(configStatusMongolices)}`, result => {
+        let data = ''
+        result.on('error', err => console.log(err))
+        result.on('data', data_ => data += data_)
+        result.on('end', () => {
+            const status = JSON.parse(data)
 
+            const pages = Math.floor(status.pageInfo.totalResults)
+            const randomPage = getRandomInt(1, pages)
+            const config = {
+                part: 'contentDetails',
+                playlistId: process.env.YOUTUBE_PLAYLIST_ID,
+                key: process.env.YOUTUBE_API_KEY
+            }
+            let pageToken
+            for (let i = 0; i < randomPage; i = i + 1) {
+                config.pageToken = pageToken
+                https.get(`https://www.googleapis.com/youtube/v3/playlistItems/?${stringify(config)}`, response => {
+                    let data = ''
+                    response.on('error', err => console.log(err))
+                    response.on('data', data_ => data += data_)
+                    response.on('end', () => {
+                        const result = JSON.parse(data)
+                        if (i == randomPage - 1) {
+                            const randomVideoNumber = getRandomInt(1, result.items.length)
+                            botInstance.sendMessage(msg.chat.id, `https://www.youtube.com/watch?v=${result.items[randomVideoNumber].contentDetails.videoId}`)
+                        } else {
+                            pageToken = result.nextPageToken
+                        }
+                    })
+                })
+            }
+        })
+    })
 }
